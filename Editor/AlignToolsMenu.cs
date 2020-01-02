203fe300f2af96ddf6b7af685b43ade28cdbe71c
litefeel <litefeel@gmail.com>
2020-01-03 00:26:46 +0800

    Update menu item and shortcut

--- Editor/AlignToolsMenu.cs
+++ Editor/AlignToolsMenu.cs
@@ -1,20 +1,13 @@
 using UnityEditor;
-#if UNITY_2019_1_OR_NEWER
-using UnityEditor.ShortcutManagement;
-using UnityEngine;
-#endif
+
 
 namespace litefeel.AlignTools
 {
     public static class AlignToolsMenu
     {
-#if UNITY_2019_1_OR_NEWER
-        private const string KeyboardMenuPath = "Align Tools/Adjust Position By Keyboard";
-        private const string WindowMenuPath = "Window/LiteFeel/Align Tools";
-#else
         private const string KeyboardMenuPath = "Window/LiteFeel/Align Tools/Adjust Position By Keyboard %#K";
         private const string WindowMenuPath = "Window/LiteFeel/Align Tools/Align Tools";
-#endif
+
         // Creation of window
         [MenuItem(WindowMenuPath)]
         private static void AlignToolsWindows()
@@ -24,14 +17,6 @@ namespace litefeel.AlignTools
             window.autoRepaintOnSceneChange = true;
         }
 
-#if UNITY_2019_1_OR_NEWER
-        [ClutchShortcut(KeyboardMenuPath, KeyCode.K, ShortcutModifiers.Action | ShortcutModifiers.Shift)]
-        private static void ToggleKeyboard(ShortcutArguments arg)
-        {
-            if (arg.stage == ShortcutStage.Begin)
-                Settings.AdjustPositionByKeyboard = !Settings.AdjustPositionByKeyboard;
-        }
-#else
         [MenuItem(KeyboardMenuPath, true)]
         private static bool VaildToggleKeyboard()
         {
@@ -43,7 +28,6 @@ namespace litefeel.AlignTools
         {
             Settings.AdjustPositionByKeyboard = !Menu.GetChecked(KeyboardMenuPath);
         }
-#endif
     }
 }
 
